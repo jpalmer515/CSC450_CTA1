e192eeac94b393fbc572a58cc56a7f62ddfaf3c1
jakepalmer <jpalmer515@gmail.com>
2024-12-14 12:37:31 -0700

    First iteration of CTA1

--- fictionalPerson.cpp
+++ fictionalPerson.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 int main() {
 
@@ -8,30 +9,52 @@ int main() {
     std::string city;
     int zipCode;
 
-    std::cout << "Enter first name: ";
-    std::cin >> firstName;
-    while (!(std::cin >> firstName)) {
-        std::cin.clear();
-        std::cin.ignore(100, '\n');
-        std::cout << "Please enter a valid first name: ";
+    while (true) {
+        std::cout << "Enter first name: ";
         std::cin >> firstName;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Wrong inputs type. Please enter a valid first name. ";
+        }
+
+        std::cout << "Enter last name: ";
+        std::cin >> lastName;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Wrong inputs type. Please enter a valid last name. ";
+        }
+
+        std::cout << "Enter address: ";
+        std::cin >> address;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Wrong inputs type. Please enter a valid address. ";
+        }
+
+        std::cout << "Enter city: ";
+        std::cin >> city;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Wrong inputs type. Please enter a valid city. ";
+        }
+
+        std::cout << "Enter zip code: ";
+        std::cin >> zipCode;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Wrong inputs type. Please enter a valid zip code using only numbers. ";
+        }
+
+        std::cout << "1. First Name: " << firstName << "\n";
+        std::cout << "2. Last Name: " << lastName << "\n";
+        std::cout << "3. Address: " << address << "\n";
+        std::cout << "4. City: " << city << "\n";
+        std::cout << "5. Zip Code: " << zipCode << "\n";
+        return 0;
     }
-
-    std::cout << "Enter last name: ";
-    std::cin >> lastName;
-
-    std::cout << "Enter address: ";
-    std::cin >> address;
-
-    std::cout << "Enter city: ";
-    std::cin >> city;
-
-    std::cout << "Enter zip code: ";
-    std::cin >> zipCode;
-
-    std::cout << "1. First Name: " << firstName << "\n";
-    std::cout << "2. Last Name: " << lastName << "\n";
-    std::cout << "3. Address: " << address << "\n";
-    std::cout << "4. City: " << city << "\n";
-    std::cout << "5. Zip Code: " << zipCode << "\n";
 }
